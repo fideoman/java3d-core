9011518a2d979143079ed7975b24e4e9784a9d25
phil <philjord@ihug.co.nz>
2016-11-03 21:05:47 +1300

    gl2es2 raster ops cannot be enabled warning message

--- src/main/java/org/jogamp/java3d/Jogl2es2DEPPipeline.java
+++ src/main/java/org/jogamp/java3d/Jogl2es2DEPPipeline.java
@@ -38,6 +38,7 @@ abstract class Jogl2es2DEPPipeline extends Pipeline
 			+ "Texture Lod, Filter, Sharpen and Combine cannot be supported. \n"//
 			+ "Texture3D cannot be supported. \n"//
 			+ "Accum style anti-aliasing cannot be supported. \n"//
+			+ "RasterOps from RenderingAttributes cannot be used. \n"//
 			+ "It is strongly recomended that you use the format GeometryArray.USE_NIO_BUFFER = true. \n"//
 			+ "Note LineArray and LineStripArray will not render as nicely as the fixed function pipeline.";//
 
